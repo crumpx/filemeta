5d9c82321bc69dd171c9dd166eaabd0858f05fdc
crumpx <crumpx@gmail.com>
2016-11-04 00:19:41 -0400

    done

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var upload = multer({dest: 'uploads/'});
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, function(){
+	console.log('server running...');
+});
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.post('/', upload.single('file'), function(req, res, next){
+  res.send({size:req.file.size});
+});
