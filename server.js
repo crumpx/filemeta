2c0b64128240f746cb57ffdee2f2b55429caefc6
crumpx <crumpx@gmail.com>
2016-11-04 00:35:09 -0400

    done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, function(){
+app.listen(process.env.PORT || 8080, function(){
 	console.log('server running...');
 });
 
